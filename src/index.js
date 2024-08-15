b770031278a7ab8daacaba77eff29fc7dd01c4a8
NikitaNimse <nimsenikita710>
2024-08-15 14:08:07 +0530

    initial setup

---
+++ src/index.js
@@ -0,0 +1,45 @@
+import "./global.css"
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import{
+  createBrowserRouter,
+  RouterProvider,
+ } from "react-router-dom"
+
+ import Home from './views/Home/Home';
+ import Login from './views/Login/Login';
+ import SignUp from './views/SignUp/SignUp';
+ import AddBlog from "./views/AddBlog/AddBlog";
+
+
+
+ const root = ReactDOM.createRoot(document.getElementById("root"))
+ const router =  createBrowserRouter([
+     {
+         path:"/",
+         element:< Home/>
+     },
+     {
+        path:"/login",
+        element:<Login/>
+    },
+    {
+        path:"/signup",
+        element:<SignUp/>
+    },
+    {
+        path: '/add-blog',
+        element: <AddBlog />
+      },
+
+
+
+     {
+      path:"*",
+      element:<> 404 not found</>
+  }
+])
+root.render(<RouterProvider router={router}/>)
+
+
